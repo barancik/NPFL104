e018bab66cb4648d5e9bcb616d9e76cf5d138147
PB <barp@centrum.cz>
2015-03-30 09:22:41 +0200

    categorical dataset

---
+++ hw03/perceptron.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+import numpy as np
+
+#Conversions
+size = {"small":-1,"big":"1"}
+color = {"red":
+
+#blog.dbrgn.ch/2013/3/26/perceptrons-in-python/
--- hw04/plot.py
+++ hw04/plot.py
@@ -15,38 +15,46 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.lda import LDA
 from sklearn.qda import QDA
 
-#adult_data
-1 - workclass
-3 - education: 
-5 - marital-status
-6 - occupation
-7 - relationship
-8 - race
-9 -sex
-13 - native-country
-14 - co chcem, result
-categorical_columns=[1,3,5,6,7,8,9,13,14]
+def adult_data():
+    names = ("age, workclass, fnlwgt, education, education-num, "
+             "marital-status, occupation, relationship, race, sex, "
+             "capital-gain, capital-loss, hours-per-week, "
+             "native-country, income").split(', ')
+    train=pd.read_csv("adult.data", names=names,delimiter=", ").dropna()
+    test=pd.read_csv("adult.test", names=names,skiprows=1,delimiter=", ").dropna()
+    y_train=train.income.apply( lambda x: 0 if x=="<=50K" else 1)
+    y_test=test.income.apply( lambda x: 0 if x=="<=50K." else 1)
 
-data=np.array(np.genfromtxt('dataset.txt', dtype=(int,'S32',int,'S32',int,'S32','S32',\ 'S32','S32','S32',int,int,int,'S32','S32'),delimiter=',',autostrip=True))
+    X_train=train.drop("income",1).T.to_dict().values()
+    X_test=test.drop("income",1).T.to_dict().values()
 
+    v = DictVectorizer(sparse=False)
+    X_train_encoded=v.fit_transform(train_X)
+    X_test_encoded=v.transform(test_X)
+    return train_encoded_X, train_y, test_encoded_X, test_y
 
-#credit_data
-train_data=np.loadtxt("credit.train.txt",delimiter=",")
-test_data=np.loadtxt("credit.test.txt",delimiter=",")
-X_train,y_train=train_data[:,:-1],train_data[:,-1]
-X_test,y_test=test_data[:,:-1],test_data[:,-1]
+def credit_data():
+    train_data=np.loadtxt("credit.train.txt",delimiter=",")
+    test_data=np.loadtxt("credit.test.txt",delimiter=",")
+    X_train,y_train=train_data[:,:-1],train_data[:,-1]
+    X_test,y_test=test_data[:,:-1],test_data[:,-1]
+    return X_train, y_train, X_test, y_test
 
-#cloud_data
-train_data=np.loadtxt("clouds_dataset/clouds1k_train.csv",skiprows=1,converters = {0: lambda x:0},delimiter=", ")
-test_data=np.loadtxt("clouds_dataset/clouds1k_test.csv",skiprows=1,converters = {0: lambda x:0},delimiter=", ")
-X_train, y_train = train_data[:,1:-1],train_data[:,-1]
-X_test, y_test = test_data[:,1:-1],test_data[:,-1]
+def cloud_data():
+    train_data=np.loadtxt("clouds_dataset/clouds1k_train.csv",skiprows=1,converters = {0: lambda x:0},delimiter=", ")
+    test_data=np.loadtxt("clouds_dataset/clouds1k_test.csv",skiprows=1,converters = {0: lambda x:0},delimiter=", ")
+    X_train, y_train = train_data[:,1:-1],train_data[:,-1]
+    X_test, y_test = test_data[:,1:-1],test_data[:,-1]
+    return X_train, y_train, X_test, y_test
+
+#aesop data
+def aesop_data():
+    data=pd.read_csv("aesop",skiprows=1)
 
 
 names = ["Nearest Neighbors", "Linear SVM", "RBF SVM", "Decision Tree",
          "Random Forest", "AdaBoost", "Naive Bayes", "LDA", "QDA"]
-
-classifiers = [
+#classifiers = [adult_data(), 
     KNeighborsClassifier(3),
     SVC(kernel="linear", C=0.025),
     SVC(gamma=2, C=1),
@@ -56,6 +64,8 @@ classifiers = [
     GaussianNB(),
     LDA(),
     QDA()]
+
+datasets=[ adult_data(),credit_data(),cloud_data()]
 
 for name,clf in zip(names,classifiers):
     clf.fit(X_train, y_train)
