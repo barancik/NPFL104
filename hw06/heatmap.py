6b3bccc42edc8ff557fcaa3b7fe37df07d48cd61
Petra Barancikova <barp@centrum.cz>
2015-04-28 15:01:05 +0200

    heatmap

--- hw06/heatmap.py
+++ hw06/heatmap.py
@@ -3,13 +3,14 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import multiprocessing
 from matplotlib.colors import Normalize
-
 from sklearn.svm import SVC
 from sklearn.preprocessing import StandardScaler
-from sklearn.datasets import load_iris
 from sklearn.cross_validation import StratifiedShuffleSplit, cross_val_score
 from sklearn.grid_search import GridSearchCV
+
+CPUS=multiprocessing.cpu_count()
 
 # Utility function to move the midpoint of a colormap to be around
 # the values of interest.
@@ -33,32 +34,29 @@ def normalize(X_train, X_test):
 def pamap_easy():
     train=np.loadtxt("pamap_easy.train.txt")
     test=np.loadtxt("pamap_easy.test.txt")
-    #dam tam zpocatku nuly, abych si vystacila s jedinou tetou
     X_train, y_train = train[:,:-1],train[:,-1]
     X_test, y_test =test[:,:-1], test[:,-1]
     X_train, X_test = normalize(X_train, X_test)
     return X_train , y_train, X_test, y_test
 
-X , y, X_test, y_test = pamap_easy()
-
 #Cross-validate to choose between linear, poly and RBF.
-def kernel_cv(X,y,cv=4):
+def kernel_cv(X,y,cv=3):
     kernels=["linear","poly","rbf"]
     scores={}
     for x in kernels:
         clf = SVC(kernel=x) #', C=1)
         score = cross_val_score(clf, X, y, cv=cv)
-        scores[x]=sum(scores)/10
+        scores[x]=sum(score)/cv.n_iter
     return max(scores, key=scores.get)
     
 #Create the heatmap for RBF
-def heat_map(X,y,cv=2):
+def heat_map(X,y,cv=2,n_jobs=CPUS):
     C_range = np.logspace(0, 7, 8)
     gamma_range = np.logspace(-3, 3, 7)
     param_grid = dict(gamma=gamma_range, C=C_range)
-    grid = GridSearchCV(SVC(), param_grid=param_grid, cv=cv)
+    grid = GridSearchCV(SVC(), param_grid=param_grid, cv=cv, n_jobs=n_jobs)
     grid.fit(X, y)
-ip
+    return grid,gamma_range,C_range
 
 
 ##############################################################################
@@ -66,34 +64,30 @@ ip
 #
 # For an initial search, a logarithmic grid with basis
 
-
-
-print("The best parameters are %s with a score of %0.2f"
-      % (grid.best_params_, grid.best_score_))
-
-# Now we need to fit a classifier for all parameters in the 2d version
-# (we use a smaller set of parameters here because it takes a while to train)
-
-C_2d_range = [1e-2, 1, 1e2]
-gamma_2d_range = [1e-1, 1, 1e1]
-classifiers = []
-for C in C_2d_range:
-    for gamma in gamma_2d_range:
-        clf = SVC(C=C, gamma=gamma)
-        clf.fit(X_2d, y_2d)
-        classifiers.append((C, gamma, clf))
-
-scores = [x[1] for x in grid.grid_scores_]
-scores = np.array(scores).reshape(len(C_range), len(gamma_range))
-
-plt.figure(figsize=(8, 6))
-plt.subplots_adjust(left=.2, right=0.95, bottom=0.15, top=0.95)
-plt.imshow(scores, interpolation='nearest', cmap=plt.cm.hot,
+if __name__ == "__main__":
+    X , y, X_test, y_test = pamap_easy()
+    cv = StratifiedShuffleSplit(y, n_iter=5, train_size=0.5, test_size=0.5)
+    print "1. Cross-validate to choose between linear, poly and RBF:",
+    cv = StratifiedShuffleSplit(y, n_iter=3, train_size=0.01, test_size=0.005)
+    print kernel_cv(X,y,cv=cv)
+    print "2. Creating the heatmap, for speedup on very small dataset - the results may vary..."
+    grid,gamma_range,C_range=heat_map(X,y,cv=cv)
+    scores = [x[1] for x in grid.grid_scores_]
+    scores = np.array(scores).reshape(len(C_range), len(gamma_range))
+    plt.figure(figsize=(8, 7))
+    plt.subplots_adjust(left=.2, right=0.95, bottom=0.15, top=0.95)
+    plt.imshow(scores, interpolation='nearest', cmap=plt.cm.hot,
            norm=MidpointNormalize(vmin=0.2, midpoint=0.92))
-plt.xlabel('gamma')
-plt.ylabel('C')
-plt.colorbar()
-plt.xticks(np.arange(len(gamma_range)), gamma_range, rotation=45)
-plt.yticks(np.arange(len(C_range)), C_range)
-plt.title('Validation accuracy')
-plt.show()
+    plt.xlabel('gamma')
+    plt.ylabel('C')
+    plt.colorbar()
+    plt.xticks(np.arange(len(gamma_range)), gamma_range, rotation=45)
+    plt.yticks(np.arange(len(C_range)), C_range)
+    plt.title('Validation accuracy')
+    plt.show()
+    print("3. the best score: {} with parameters, gamma: {}, C:{}".format(grid.best_score_,
+           grid.best_params_["gamma"], grid.best_params_["C"]))
+
+   
+
+
